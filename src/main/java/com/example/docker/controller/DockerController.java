6238661cfb1148c83b6be495a09422f677aa2b41
zf178073575 <178073575@qq.com>
2020-07-14 19:19:19 +0800

    1

--- src/main/java/com/example/docker/controller/DockerController.java
+++ src/main/java/com/example/docker/controller/DockerController.java
@@ -11,6 +11,6 @@ public class DockerController {
 
     @RequestMapping("/get")
     public String getStr(){
-        return "Hi dafei "+new Date();
+        return "Hi dafei---------- "+new Date();
     }
 }
