d63dfc746310cf47796fd8625b13ea9942b868af
zf178073575 <178073575@qq.com>
2020-07-14 15:45:17 +0800

    1

--- src/main/java/com/example/docker/controller/DockerController.java
+++ src/main/java/com/example/docker/controller/DockerController.java
@@ -3,12 +3,14 @@ package com.example.docker.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 @RequestMapping("/docker")
 public class DockerController {
 
     @RequestMapping("/get")
     public String getStr(){
-        return "Hello docker!";
+        return "Hello docker!!!"+new Date();
     }
 }
